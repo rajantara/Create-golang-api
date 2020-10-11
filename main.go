79200a08af2c9cebd574249db3547297b7aa2d05
rajantara <rajanpensas@gmail.com>
2020-10-11 17:41:26 +0800

    update data

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	"encoding/json"
+	"github.com/gorilla/mux"
 )
 
 type Article struct {
@@ -22,7 +23,6 @@ func allArticles(w http.ResponseWriter, r *http.Request)  {
 		Article{Name:"rajan", Email:"rajan@gmail.com", Phone:"0823547556"},
 	}
 
-
 	fmt.Println("Endpoint hit  All Articles Endpoint")
 	json.NewEncoder(w).Encode(articles)
 }
@@ -31,10 +31,17 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w,"home page endpoint hit")
 }
 
+func testPostArticles(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w,"Test Endpoint Worked")
+}
+
 func handleRequest(){
-	http.HandleFunc("/", homePage)
-	http.HandleFunc("/articles", allArticles)
-	log.Fatal(http.ListenAndServe(":8081", nil))
+
+	myRouter := mux.NewRouter().StrictSlash(true)
+	myRouter.HandleFunc("/", homePage)
+	myRouter.HandleFunc("/articles", allArticles).Methods("GET")
+	myRouter.HandleFunc("/articles", testPostArticles).Methods("POST")
+	log.Fatal(http.ListenAndServe(":8081", myRouter))
 }
 
 func main() {
