885b88d0ee6294962342175bc35c5a641806e812
rajantara <rajanpensas@gmail.com>
2020-10-11 16:55:31 +0800

    test rest api

--- main.go
+++ main.go
@@ -2,9 +2,41 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+	"encoding/json"
 )
 
+type Article struct {
+	Name string `json:"name"`
+	Email string `json:"email"`
+	Phone string `json:"phone"`
+
+}
+
+type Articles []Article
+
+func allArticles(w http.ResponseWriter, r *http.Request)  {
+
+	articles := Articles {
+		Article{Name:"rajan", Email:"rajan@gmail.com", Phone:"0823547556"},
+	}
+
+
+	fmt.Println("Endpoint hit  All Articles Endpoint")
+	json.NewEncoder(w).Encode(articles)
+}
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w,"home page endpoint hit")
+}
+
+func handleRequest(){
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/articles", allArticles)
+	log.Fatal(http.ListenAndServe(":8081", nil))
+}
 
 func main() {
-	fmt.Println("hello word")
+	handleRequest()
 }
